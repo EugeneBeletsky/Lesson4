9ab4f3b45f06f79daaf7b552ed5b12134fb5a4a9
EugeneBeletsky <eugenebeletsky@gmail.com>
2022-02-18 18:47:17 +0300

    added new files

---
+++ conversation.js
@@ -0,0 +1,16 @@
+let a="123";        //конвертация строки в число через parseInt
+let a1= parseInt(a,10)
+console.log(a1, typeof a1);
+
+let a2="234";       //коневертация через функцию number
+let a3= Number(a2);
+console.log(a3, typeof a3);
+
+let b=456;      //конвертация через функцию string
+let b1=String(b);
+console.log(b1, typeof b1);
+
+let c="qwe";        //конвертация через функцию boolean
+let c1=Boolean(c);
+console.log(c1, typeof c1);
+
